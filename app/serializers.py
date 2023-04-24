baabceaf8d30404f89548cc260dcaa59bcb4abaa
ALexShred <mkali2065@gmail.com>
2023-04-24 22:55:42 +0600

    change views and serializers

--- app/serializers.py
+++ app/serializers.py
@@ -3,14 +3,31 @@ from rest_framework import serializers
 from .models import Category, Product
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
+class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField()
+
+    def create(self, validated_data):
+        return Category.objects.create(
+            name=validated_data['name']
+        )
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data['name']
 
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+class ProductSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    price = serializers.CharField()
+    category_id = serializers.IntegerField()
 
+    def create(self, validated_data):
+        return Product.objects.create(
+            name=validated_data['name'],
+            price=validated_data['price'],
+            category_id=validated_data['category_id']
+        )
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data['name']
+        instance.price = validated_data['price']
+        instance.category_id = validated_data['category_id']
--- app/views.py
+++ app/views.py
@@ -1,14 +1,39 @@
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .serializers import ProductSerializer, CategorySerializer
 from .models import Product, Category
 
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+@api_view(http_method_names=['POST', 'GET'])
+def product_list_create_api_view(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(instance=products, many=True)
+        return Response(serializer.data, status=200)
+    if request.method == 'POST':
+        received_data = request.data
+        serializer = ProductSerializer(data=received_data)
+
+        if serializer.is_valid():
+            product = serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
 
 
-class CategoryListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+@api_view(http_method_names=['POST', 'GET'])
+def category_list_create_api_view(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer1 = CategorySerializer(instance=categories, many=True)
+        return Response(serializer1.data, status=200)
+    if request.method == 'POST':
+        received_data1 = request.data
+        serializer = CategorySerializer(data=received_data1)
+
+        if serializer.is_valid():
+            category = serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
